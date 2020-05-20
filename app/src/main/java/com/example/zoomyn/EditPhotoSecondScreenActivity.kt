869aae8ed5272ab7ed3c19291b589380737e0db3
Alyona Startseva <alyona_startseva.tk@mail.ru>
2020-05-21 03:23:39 +0700

    changed third activity

--- app/src/main/java/com/example/zoomyn/EditPhotoSecondScreenActivity.kt
+++ app/src/main/java/com/example/zoomyn/EditPhotoSecondScreenActivity.kt
@@ -8,5 +8,7 @@ class EditPhotoSecondScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_photo_second_screen)
+
+
     }
 }
